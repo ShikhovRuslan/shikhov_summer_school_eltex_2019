0299f23363a6f90de5ec16fdab21f9b107653691
Ruslan Shikhov <shikhovruslan@yandex.ru>
2019-09-21 04:17:04 +0700

    20.09.19 - 3

--- src/main/java/ru/eltex/app/java/lab8/controller/OrderController.java
+++ src/main/java/ru/eltex/app/java/lab8/controller/OrderController.java
@@ -8,16 +8,16 @@ import ru.eltex.app.java.lab1.TypePhone;
 import ru.eltex.app.java.lab1.TypeSIM;
 import ru.eltex.app.java.lab2.OrderStatus;
 import ru.eltex.app.java.lab7.DelException;
+import ru.eltex.app.java.lab8.exception.ResourceNotFoundException;
 import ru.eltex.app.java.lab8.model.*;
 import ru.eltex.app.java.lab8.repository.CredentialsRepository;
-import ru.eltex.app.java.lab8.repository.DeviceRepository;
 import ru.eltex.app.java.lab8.repository.OrderRepository;
-import ru.eltex.app.java.lab8.repository.ShoppingCartRepository;
 
 import java.awt.*;
 import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -25,35 +25,23 @@ public class OrderController {
 
     private static final String INFO_MESSAGE = "Checking the command entered by the user. ";
     private static final String ERROR_MESSAGE = "Sorry, something went wrong!";
-    private static final Logger logger = Logger.getLogger(ru.eltex.app.java.lab7.OrdersController.class);
+    private static final Logger logger = Logger.getLogger(ru.eltex.app.java.lab8.controller.OrderController.class);
 
     @Autowired
     private OrderRepository orderRepository;
 
     @Autowired
-    private ShoppingCartRepository cartRepository;
-
-    @Autowired
     private CredentialsRepository userRepository;
 
-    @Autowired
-    private DeviceRepository deviceRepository;
-
-    private void checkingId(String str) throws DelException {
-        try {
-            UUID id = UUID.fromString(str);
-        } catch (IllegalArgumentException e) {
-            throw new DelException(3);
-        }
-    }
-
-    private Object fillInDatabase() {
+    private String fillInDatabase() {
         List<Credentials> users = new ArrayList<>();
         List<Device> devices = new ArrayList<>();
         List<Device> devices1 = new ArrayList<>();
         List<Device> devices2 = new ArrayList<>();
-        List<ShoppingCart> carts = new ArrayList<>();
         List<Order> orders = new ArrayList<>();
+
+        orderRepository.deleteAllInBatch();
+        userRepository.deleteAllInBatch();
 
         Credentials user1 = new Credentials(
                 "surname1", "name1", "patronymic1", "name1@yandex.ru");
@@ -65,13 +53,13 @@ public class OrderController {
 
         Device phone = new Phone(TypePhone.CLASSICAL,
                 "name_phone", 14.07, "firm_phone",
-                "model_phone", "OS_phone", new ArrayList<>());
+                "model_phone", "OS_phone");
         Device smartphone = new Smartphone(TypeSIM.MICRO_SIM, 2,
                 "name_smartphone", 28.43, "firm_smartphone",
-                "model_smartphone", "OS_smartphone", new ArrayList<>());
+                "model_smartphone", "OS_smartphone");
         Device tablet = new Tablet("vp", new Dimension(),
                 "name_tablet", 28.43, "firm_tablet",
-                "model_tablet", "OS_tablet", new ArrayList<>());
+                "model_tablet", "OS_tablet");
         devices.add(phone);
         devices.add(smartphone);
         devices.add(tablet);
@@ -79,24 +67,28 @@ public class OrderController {
         devices1.add(tablet);
         devices2.add(phone);
         devices2.add(smartphone);
-        deviceRepository.saveAll(devices);
 
-        ShoppingCart cart1 = new ShoppingCart(new ArrayList<Device>());
-        ShoppingCart cart2 = new ShoppingCart(new ArrayList<Device>());
+        ShoppingCart cart1 = new ShoppingCart();
+        ShoppingCart cart2 = new ShoppingCart();
+
         for (Device device : devices1) {
-            cart1.getProducts().add(phone);
-            phone.getCarts().add(cart1);
+            cart1.add(device);
+            device.getCarts().add(cart1);
         }
         for (Device device : devices2) {
-            cart2.getProducts().add(device);
+            cart2.add(device);
             device.getCarts().add(cart2);
         }
-        carts.add(cart1);
-        carts.add(cart2);
-        cartRepository.saveAll(carts);
 
-        Order order1 = new Order(OrderStatus.WAITING, new Date(System.currentTimeMillis()), 17L, cart1, user1);
-        Order order2 = new Order(OrderStatus.WAITING, new Date(System.currentTimeMillis()), 17L, cart2, user2);
+        Order order1 = new Order(OrderStatus.WAITING, new Date(System.currentTimeMillis()), 17L, user1);
+        Order order2 = new Order(OrderStatus.WAITING, new Date(System.currentTimeMillis()), 17L, user2);
+        order1.setId("00000000-0000-0000-0000-000000000021");
+
+        order1.setCart(cart1);
+        order2.setCart(cart2);
+        cart1.setOrder(order1);
+        cart2.setOrder(order2);
+
         orders.add(order1);
         orders.add(order2);
         orderRepository.saveAll(orders);
@@ -104,20 +96,16 @@ public class OrderController {
         return "Добавлены заказы в количестве " + orders.size() + " ед.";
     }
 
-    private Object getAllOrders() {
-        deviceRepository.findAll();
-        cartRepository.findAll();
-        userRepository.findAll();
-        orderRepository.findAll();
+    private List<Order> getAllOrders() {
         return orderRepository.findAll();
     }
 
-    private Object getOrderById(String orderId) {
-        deviceRepository.findAll();
-        cartRepository.findAll();
-        userRepository.findAll();
-        orderRepository.findAll();
-        return orderRepository.findById(orderId);
+    private Optional<Order> getOrderById(String orderId) throws ResourceNotFoundException {
+        Optional<Order> order = orderRepository.findById(orderId);
+        if (order.isPresent()) {
+            return order;
+        }
+        throw new ResourceNotFoundException("Заказ с ID=" + orderId + " не найден!");
     }
 
     private String delOrderById(String orderId) throws DelException {
@@ -129,7 +117,7 @@ public class OrderController {
         }
     }
 
-    private String addDeviceToCart(String cartId) {
+    private String addDeviceToCart(String cartId) throws ResourceNotFoundException {
         String deviceId = "";
         List<Order> orders = orderRepository.findAll();
         boolean isDeviceAdded = false;
@@ -137,8 +125,8 @@ public class OrderController {
             if (order.getCart().getId().equals(cartId)) {
                 Device device = new Phone(TypePhone.FLIP,
                         "name_new_phone", 79.23, "firm_new_phone",
-                        "model_new_phone", "OS_new_phone", new ArrayList<>());
-                order.getCart().getProducts().add(device);
+                        "model_new_phone", "OS_new_phone");
+                order.getCart().add(device);
                 deviceId = device.getId();
                 isDeviceAdded = true;
                 break;
@@ -147,13 +135,12 @@ public class OrderController {
         if (isDeviceAdded) {
             orderRepository.saveAll(orders);
             return "Устройство с ID=" + deviceId + " добавлено в корзину с ID=" + cartId;
-        } else {
-            return "Корзина c ID=" + cartId + " не найдена!";
         }
+        throw new ResourceNotFoundException("Корзина c ID=" + cartId + " не найдена!");
     }
 
     @GetMapping(value = "/", params = {"command"})
-    public Object show1(@RequestParam("command") String command) {
+    private Object show1(@RequestParam("command") String command) {
         logger.info(INFO_MESSAGE + "\"readall\" or \"fillInDatabase\" is awaited.");
         try {
             switch (command) {
@@ -170,11 +157,12 @@ public class OrderController {
         }
     }
 
-    @RequestMapping(value = "/", method = RequestMethod.GET, params = {"command", "order_id"})
-    private Object show2(@RequestParam("command") String command, @RequestParam("order_id") String orderId) {
+    @GetMapping(value = "/", params = {"command", "order_id"})
+    private Object show2(@RequestParam("command") String command,
+                         @RequestParam("order_id") String orderId) {
         logger.info(INFO_MESSAGE + "\"readById\" or \"delById\" is awaited.");
         try {
-            checkingId(orderId);
+            checkId(orderId);
             switch (command) {
                 case ("readById"):
                     return getOrderById(orderId);
@@ -186,14 +174,18 @@ public class OrderController {
         } catch (DelException e) {
             logger.error(ERROR_MESSAGE, e);
             return "Код ошибки - " + e.getErrorCode() + ". " + e.getMessage();
+        } catch (ResourceNotFoundException e) {
+            logger.error(ERROR_MESSAGE, e);
+            return e.getMessage();
         }
     }
 
-    @RequestMapping(value = "/", method = RequestMethod.GET, params = {"command", "card_id"})
-    private Object show3(@RequestParam("command") String command, @RequestParam("card_id") String cartId) {
+    @GetMapping(value = "/", params = {"command", "card_id"})
+    private Object show3(@RequestParam("command") String command,
+                         @RequestParam("card_id") String cartId) {
         logger.info(INFO_MESSAGE + "\"addToCard\" is awaited.");
         try {
-            checkingId(cartId);
+            checkId(cartId);
             switch (command) {
                 case ("addToCard"):
                     return addDeviceToCart(cartId);
@@ -203,6 +195,9 @@ public class OrderController {
         } catch (DelException e) {
             logger.error(ERROR_MESSAGE, e);
             return "Код ошибки - " + e.getErrorCode() + ". " + e.getMessage();
+        } catch (ResourceNotFoundException e) {
+            logger.error(ERROR_MESSAGE, e);
+            return e.getMessage();
         }
     }
 
@@ -213,6 +208,14 @@ public class OrderController {
         } catch (DelException e) {
             logger.error(ERROR_MESSAGE, e);
             return "Код ошибки - " + e.getErrorCode() + ". " + e.getMessage();
+        }
+    }
+
+    private void checkId(String str) throws DelException {
+        try {
+            UUID id = UUID.fromString(str);
+        } catch (IllegalArgumentException e) {
+            throw new DelException(3);
         }
     }
 
