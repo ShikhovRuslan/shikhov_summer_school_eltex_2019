4a8f15cf242683ccc7ef54d5f53ef6dc9afd7143
Ruslan Shikhov <shikhovruslan@yandex.ru>
2019-09-21 02:19:09 +0700

    20.09.19 - 1

---
+++ src/main/java/ru/eltex/app/java/lab8/exception/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package ru.eltex.app.java.lab8.exception;
+
+public class ResourceNotFoundException extends Exception {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+}
